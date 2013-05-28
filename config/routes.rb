61abdca4f09f212e57cfb08c77590bc962b40cf9
cllggg <cllggg@hotmail.com>
2013-05-28 18:04:44 +0800

    commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 MyWeb::Application.routes.draw do
+  resources :microposts
+
+
+  resources :users
+
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -55,7 +61,5 @@ MyWeb::Application.routes.draw do
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id))(.:format)'
-
-  root :to => 'home#index'
 
 end
